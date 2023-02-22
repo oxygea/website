22780978ce4c43b798e2bc21e39dc67138632cca
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2023-02-22 16:49:25 -0300

    structure cover

--- components/Home/Cover.vue
+++ components/Home/Cover.vue
@@ -1,5 +1,27 @@
 <template>
-  <section>qwe</section>
+  <section class="bg-black">
+    <nuxt-img
+      preload
+      :src="`cover.png`"
+      format="webp"
+      fit="fill"
+      quality="100"
+      loading="lazy"
+      sizes="83px sm:166px lg:300px"
+      :alt="`Imagem cover`"
+      :title="`Cover`"
+      class="w-full h-auto"
+    />
+    <div class="container !py-10">
+      <p class="font-normal text-[28px] leading-8 pb-6 text-white">
+        Nos conectamos com parceiros de referência global e mentores
+        especializados -
+      </p>
+      <p class="font-bold text-xl text-white leading-6">
+        gente que acorda e dorme pensando inovação, como você.
+      </p>
+    </div>
+  </section>
 </template>
 <script>
 export default {
--- components/Home/Startup.vue
+++ components/Home/Startup.vue
@@ -1,5 +1,24 @@
 <template>
-  <section>qwe</section>
+  <section class="bg-[#EDEDEF]">
+    <nuxt-img
+      preload
+      :src="`startup.png`"
+      format="webp"
+      fit="fill"
+      quality="100"
+      loading="lazy"
+      sizes="83px sm:166px lg:300px"
+      :alt="`Imagem startup`"
+      :title="`Startup`"
+      class="w-full h-auto"
+    />
+    <div class="container !py-10">
+      <p class="font-normal text-[28px] leading-8 pb-6">
+        Entre nossos programas, um deles é pra você.
+      </p>
+      <p class="font-bold text-xl">Conheça e faça parte.</p>
+    </div>
+  </section>
 </template>
 <script>
 export default {
