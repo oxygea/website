fc8f010edf040bde874750f50e18cc02f23ec222
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2023-02-22 18:04:31 -0300

    feat: section numbers

--- components/Home/BigNumbers.vue
+++ components/Home/BigNumbers.vue
@@ -1,5 +1,41 @@
 <template>
-  <section>qwe</section>
+  <section
+    class="container bg-white flex flex-wrap items-center justify-center !pt-10"
+  >
+    <div
+      class="w-full max-w-[180px] h-[180px] rounded-full border-2 border-violet flex flex-col gap-2 items-center justify-center text-center"
+    >
+      <p class="text-violet text-[28px] font-normal">U$150MM</p>
+      <p class="text-violet text-[12px] font-normal leading-4">
+        de capital <br />
+        comprometido
+      </p>
+    </div>
+    <div
+      class="relative -top-5 w-full max-w-[140px] h-[140px] rounded-full bg-gray flex flex-col gap-2 items-center justify-center text-center"
+    >
+      <p class="text-violet text-[28px] font-normal">4</p>
+      <p class="text-violet text-[12px] font-normal leading-4 max-w-[128px]">
+        empresas no portfolio
+      </p>
+    </div>
+    <div
+      class="relative -top-10 w-full max-w-[180px] h-[180px] rounded-full border-2 border-violet flex flex-col gap-2 items-center justify-center text-center"
+    >
+      <p class="text-violet text-[28px] font-normal">7</p>
+      <p class="text-violet text-[12px] font-normal leading-4 max-w-[128px]">
+        centros de pesquisa, inovação e biotecnologia
+      </p>
+    </div>
+    <div
+      class="relative -top-[60px] w-full max-w-[140px] h-[140px] rounded-full border-2 border-gray flex flex-col gap-2 items-center justify-center text-center"
+    >
+      <p class="text-violet text-[28px] font-normal">7</p>
+      <p class="text-violet text-[12px] font-normal leading-4 max-w-[128px]">
+        plantas piloto
+      </p>
+    </div>
+  </section>
 </template>
 <script>
 export default {
