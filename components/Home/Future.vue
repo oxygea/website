8f932fc542163cb29ca861a36f3311b4372dd2cf
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2023-02-22 18:38:54 -0300

    section video

--- components/Home/Future.vue
+++ components/Home/Future.vue
@@ -1,5 +1,14 @@
 <template>
-  <section>qwe</section>
+  <section>
+    <iframe
+      width="100%"
+      height="240px"
+      src="https://www.youtube.com/embed/JAXHea9as30"
+      title="Oxygea"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </section>
 </template>
 <script>
 export default {
