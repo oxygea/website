d0fafb6ad79f1a567b393c85a337afba13e6ae40
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2023-02-22 16:22:45 -0300

    structure page invest

--- components/Home/Invest.vue
+++ components/Home/Invest.vue
@@ -1,6 +1,69 @@
 <template>
   <section class="bg-green container !py-10">
     <h2 class="pb-4 font-bold text-2xl">Onde investimos</h2>
+    <div class="flex flex-wrap pt-10 gap-8">
+      <div>
+        <svg-icon name="circularEconomy" class="w-8 h-8" />
+        <h3 class="pb-2 pt-4 pt-text-base font-medium leading-5">
+          Economia Circular
+        </h3>
+        <p class="text-xs font-normal leading-4">
+          Reciclagem química/mecânica/ por dissolução, produtos de plastic
+          waste-based e new plastic replacements.<br />
+          Logística reversa.
+        </p>
+      </div>
+      <div>
+        <svg-icon name="fintech" class="w-8 h-8" />
+        <h3 class="pb-2 pt-4 pt-text-base font-medium leading-5">
+          Fintech em Economia Circular & Mudanças Climáticas
+        </h3>
+        <p class="text-xs font-normal leading-4">
+          Aplicações de fintechs no mercado de carbono e plástico. <br />
+          Marketplaces.
+        </p>
+      </div>
+      <div>
+        <svg-icon name="carbonNeutrality" class="w-8 h-8" />
+        <h3 class="pb-2 pt-4 pt-text-base font-medium leading-5">
+          Neutralidade de Carbono
+        </h3>
+        <p class="text-xs font-normal leading-4">
+          Captura, armazenamento, utilização de carbono. Outras tecnologias do
+          mercado de carbono.
+        </p>
+      </div>
+      <div>
+        <svg-icon name="biotechnology" class="w-8 h-8" />
+        <h3 class="pb-2 pt-4 pt-text-base font-medium leading-5">
+          Biotecnologia & Nanotecnologia
+        </h3>
+        <p class="text-xs font-normal leading-4">
+          Biotecnologia em químicos, plásticos e materiais. <br />
+          Nanotecnologia em manufatura.
+        </p>
+      </div>
+      <div>
+        <svg-icon name="industry" class="w-8 h-8" />
+        <h3 class="pb-2 pt-4 pt-text-base font-medium leading-5">
+          Indústria 4.0
+        </h3>
+        <p class="text-xs font-normal leading-4">
+          Inteligência artificial. Machine Learning. <br />
+          Computação quântica.
+        </p>
+      </div>
+      <div>
+        <svg-icon name="biochemicals" class="w-8 h-8" />
+        <h3 class="pb-2 pt-4 pt-text-base font-medium leading-5">
+          Bioquímicos avançados & materiais
+        </h3>
+        <p class="text-xs font-normal leading-4">
+          Polímeros biodegradáveis, monômeros bio-based. <br />
+          Aplicações leves, embalagens monomaterial.
+        </p>
+      </div>
+    </div>
   </section>
 </template>
 <script>
