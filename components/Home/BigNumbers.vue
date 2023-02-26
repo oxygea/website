e552df90b833cc7e5af71cc7a600abaddefe344a
Maurício Sousa <mauricio@bemobile.tech>
2023-02-26 18:09:14 -0300

    feat(BigNumbers): add animations

--- components/Home/BigNumbers.vue
+++ components/Home/BigNumbers.vue
@@ -4,7 +4,7 @@
       class="container flex flex-col items-center justify-center pt-10 lg:flex-row lg:items-center lg:py-20"
     >
       <div
-        class="w-full max-w-[180px] h-[180px] rounded-full border-2 border-violet flex flex-col gap-2 items-center justify-center text-center lg:top-0 xl:max-w-[310px] xl:h-[310px]"
+        class="custom-border w-full max-w-[180px] h-[180px] flex flex-col gap-2 items-center justify-center text-center lg:top-0 xl:max-w-[310px] xl:h-[310px]"
       >
         <p class="text-violet text-[28px] font-normal xl:text-5xl">U$150MM</p>
         <p
@@ -15,7 +15,7 @@
       </div>
 
       <div
-        class="relative -top-5 w-full max-w-[140px] h-[140px] rounded-full bg-gray flex flex-col gap-2 items-center justify-center text-center lg:top-0 lg:-left-3 xl:max-w-[213px] xl:h-[213px]"
+        class="custom-background relative -top-5 w-full max-w-[140px] h-[140px] flex flex-col gap-2 items-center justify-center text-center lg:top-0 lg:-left-3 xl:max-w-[213px] xl:h-[213px]"
       >
         <p class="text-violet text-[28px] font-normal xl:text-5xl">4</p>
         <p
@@ -26,7 +26,7 @@
       </div>
 
       <div
-        class="relative -top-10 w-full max-w-[180px] h-[180px] rounded-full border-2 border-violet flex flex-col gap-2 items-center justify-center text-center lg:top-0 lg:-left-5 xl:max-w-[310px] xl:h-[310px]"
+        class="custom-border relative -top-10 w-full max-w-[180px] h-[180px] flex flex-col gap-2 items-center justify-center text-center lg:top-0 lg:-left-5 xl:max-w-[310px] xl:h-[310px]"
       >
         <p class="text-violet text-[28px] font-normal xl:text-5xl">7</p>
         <p
@@ -37,7 +37,7 @@
       </div>
 
       <div
-        class="relative -top-[60px] w-full max-w-[140px] h-[140px] rounded-full border-2 border-gray flex flex-col gap-2 items-center justify-center text-center lg:top-0 lg:-left-7 xl:max-w-[213px] xl:h-[213px]"
+        class="custom-border custom-border--gray relative -top-[60px] w-full max-w-[140px] h-[140px] flex flex-col gap-2 items-center justify-center text-center lg:top-0 lg:-left-7 xl:max-w-[213px] xl:h-[213px]"
       >
         <p class="text-violet text-[28px] font-normal xl:text-5xl">7</p>
         <p
@@ -54,3 +54,56 @@ export default {
   name: 'HomeBigNumbers',
 }
 </script>
+
+<style lang="postcss" scoped>
+.custom-border {
+  position: relative;
+
+  &::before {
+    @apply transition-transform;
+    @apply rounded-full;
+    @apply border-2;
+    @apply border-violet;
+
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+
+  &--gray::before {
+    @apply border-gray;
+  }
+
+  &:hover::before {
+    @apply scale-110;
+  }
+}
+
+.custom-background {
+  position: relative;
+
+  p {
+    z-index: 2;
+  }
+
+  &::before {
+    @apply transition-transform;
+    @apply rounded-full;
+    @apply bg-gray;
+
+    position: absolute;
+    content: '';
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover::before {
+    @apply scale-110;
+  }
+}
+</style>
