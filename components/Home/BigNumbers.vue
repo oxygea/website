aa9fc799a7f5c55adc672d79e2af1fdc7db7ad87
Maurício Sousa <mauricio@bemobile.tech>
2023-02-26 15:54:40 -0300

    fix(BigNumbers): adjust mobile

--- components/Home/BigNumbers.vue
+++ components/Home/BigNumbers.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="container bg-white flex flex-wrap items-center justify-center !pt-10"
+    class="container bg-white flex flex-col items-center justify-center !pt-10"
   >
     <div
       class="w-full max-w-[180px] h-[180px] rounded-full border-2 border-violet flex flex-col gap-2 items-center justify-center text-center"
@@ -10,6 +10,7 @@
         {{ $t('numbers.capital') }}
       </p>
     </div>
+
     <div
       class="relative -top-5 w-full max-w-[140px] h-[140px] rounded-full bg-gray flex flex-col gap-2 items-center justify-center text-center"
     >
@@ -18,14 +19,16 @@
         {{ $t('numbers.companies') }}
       </p>
     </div>
+
     <div
       class="relative -top-10 w-full max-w-[180px] h-[180px] rounded-full border-2 border-violet flex flex-col gap-2 items-center justify-center text-center"
     >
       <p class="text-violet text-[28px] font-normal">7</p>
-      <p
-        class="text-violet text-[12px] font-normal leading-4 max-w-[128px]"
-      ></p>
+      <p class="text-violet text-[12px] font-normal leading-4 max-w-[128px]">
+        {{ $t('numbers.search') }}
+      </p>
     </div>
+
     <div
       class="relative -top-[60px] w-full max-w-[140px] h-[140px] rounded-full border-2 border-gray flex flex-col gap-2 items-center justify-center text-center"
     >
