19daff328a4306f12433ffcba624726771c53ce5
Maurício Sousa <mauricio@bemobile.tech>
2023-02-26 11:20:26 -0300

    feat(Mentors): init responsivity

--- components/Home/Mentors.vue
+++ components/Home/Mentors.vue
@@ -1,102 +1,111 @@
 <template>
   <section class="bg-white !py-10">
     <div class="container">
-      <h2 class="pb-4 font-bold text-2xl">Mentores</h2>
-      <p class="font-normal text-xs leading-4 pb-10">
+      <h2 class="pb-4 font-bold text-2xl lg:text-[42px]">Mentores</h2>
+      <p class="font-normal text-xs leading-4 pb-10 lg:text-base lg:ml-[110px]">
         Especialistas reconhecidos em diversos campos do conhecimento colaboram
         com a gente. Referências da indústria, da ciência, da pesquisa, da
         inovação e da sustentabilidade, com a missão de oferecer a profundidade
         necessária para mudar o mercado.
       </p>
-      <p class="font-bold text-xs">FILTRAR POR</p>
     </div>
-    <VueSlickCarousel
-      v-bind="slickOptions"
-      class="pl-5 pb-10 pt-2 flex items-center w-full"
-    >
-      <div
-        class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px]"
-        :class="[
-          {
-            'bg-black': selectFilter === 'All',
-            'text-white': selectFilter === 'All',
-          },
-        ]"
-        @click.prevent="selectFilter = 'All'"
-      >
-        Todos
+
+    <div class="lg:flex lg:items-center lg:mb-10">
+      <div class="container lg:pr-0">
+        <p class="font-bold text-xs lg:ml-[110px] w-full">FILTRAR POR</p>
       </div>
-      <div
-        class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px]"
-        :class="[
-          {
-            'bg-black': selectFilter === 'Sustainability',
-            'text-white': selectFilter === 'Sustainability',
-          },
-        ]"
-        @click.prevent="selectFilter = 'Sustainability'"
+
+      <VueSlickCarousel
+        v-bind="slickOptions"
+        class="flex items-center w-full container !pr-0 !pb-10 !pt-2 lg:!p-0 lg:ml-14"
       >
-        Sustentabilidade
-      </div>
-      <div
-        class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px]"
-        :class="[
-          {
-            'bg-black': selectFilter === 'Technology',
-            'text-white': selectFilter === 'Technology',
-          },
-        ]"
-        @click.prevent="selectFilter = 'Technology'"
-      >
-        Tecnologia
-      </div>
-      <div
-        class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px]"
-        :class="[
-          {
-            'bg-black': selectFilter === 'Biochemistry',
-            'text-white': selectFilter === 'Biochemistry',
-          },
-        ]"
-        @click.prevent="selectFilter = 'Biochemistry'"
-      >
-        Bioquímica
-      </div>
-      <div
-        class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px]"
-        :class="[
-          {
-            'bg-black': selectFilter === 'HumanResources',
-            'text-white': selectFilter === 'HumanResources',
-          },
-        ]"
-        @click.prevent="selectFilter = 'HumanResources'"
-      >
-        Recursos Humanos
-      </div>
-    </VueSlickCarousel>
-    <div class="w-full flex flex-wrap container">
-      <div
-        class="grid grid-cols-3 w-full items-center py-4 border-y border-gray"
-      >
-        <p class="font-medium text-xs">Jorge Soto</p>
-        <nuxt-img
-          preload
-          :src="`defaultMentor.png`"
-          format="webp"
-          fit="fill"
-          quality="100"
-          loading="lazy"
-          sizes="100px sm:100vw lg:300px"
-          :alt="`Imagem team ${c}`"
-          :title="`Team ${c}`"
-          class="w-16 h-16 m-auto"
-        />
         <div
-          class="flex justify-between items-center min-w-max gap-2 cursor-pointer"
+          class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px] mr-2"
+          :class="[
+            {
+              'bg-black': selectFilter === 'All',
+              'text-white': selectFilter === 'All',
+            },
+          ]"
+          @click.prevent="selectFilter = 'All'"
         >
-          <p class="font-bold text-sm">VER MAIS</p>
-          <svg-icon name="arrowCircle" class="w-6 h-6" />
+          Todos
+        </div>
+        <div
+          class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px] mr-2"
+          :class="[
+            {
+              'bg-black': selectFilter === 'Sustainability',
+              'text-white': selectFilter === 'Sustainability',
+            },
+          ]"
+          @click.prevent="selectFilter = 'Sustainability'"
+        >
+          Sustentabilidade
+        </div>
+        <div
+          class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px] mr-2"
+          :class="[
+            {
+              'bg-black': selectFilter === 'Technology',
+              'text-white': selectFilter === 'Technology',
+            },
+          ]"
+          @click.prevent="selectFilter = 'Technology'"
+        >
+          Tecnologia
+        </div>
+        <div
+          class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px] mr-2"
+          :class="[
+            {
+              'bg-black': selectFilter === 'Biochemistry',
+              'text-white': selectFilter === 'Biochemistry',
+            },
+          ]"
+          @click.prevent="selectFilter = 'Biochemistry'"
+        >
+          Bioquímica
+        </div>
+        <div
+          class="border-solid border border-black py-2 px-4 text-xs font-bold min-w-max max-w-max rounded-[100px] mr-2"
+          :class="[
+            {
+              'bg-black': selectFilter === 'HumanResources',
+              'text-white': selectFilter === 'HumanResources',
+            },
+          ]"
+          @click.prevent="selectFilter = 'HumanResources'"
+        >
+          Recursos Humanos
+        </div>
+      </VueSlickCarousel>
+    </div>
+
+    <div class="container">
+      <div class="w-full flex flex-wrap">
+        <div
+          class="grid grid-cols-3 w-full items-center py-4 border-y border-gray"
+        >
+          <p class="font-medium text-xs">Jorge Soto</p>
+          <nuxt-img
+            preload
+            :src="`defaultMentor.png`"
+            format="webp"
+            fit="fill"
+            quality="100"
+            loading="lazy"
+            sizes="100px sm:100vw lg:300px"
+            :alt="`Imagem team ${c}`"
+            :title="`Team ${c}`"
+            class="w-16 h-16 m-auto"
+          />
+          <div
+            class="flex justify-between items-center min-w-max gap-2 cursor-pointer"
+          >
+            <p class="font-bold text-sm">VER MAIS</p>
+            <svg-icon name="arrowCircle" class="w-6 h-6" />
+          </div>
         </div>
       </div>
     </div>
@@ -109,10 +118,11 @@ export default {
     return {
       slickOptions: {
         dots: false,
-        slidesToShow: 2.7,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: false,
         arrows: false,
+        variableWidth: true,
       },
       selectFilter: 'All',
     }
