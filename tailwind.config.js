abd562b5a6ec082e90cdf680fdf6860ccad70854
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2023-02-27 13:36:17 -0300

    feat: fonts config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
     fontFamily: {
       base: ['Acrom', 'sans-serif'],
     },
+    fontSize: {
+      xxs: ['10px', { lineHeight: '14px' }],
+      xs: ['12px', { lineHeight: '16px' }],
+      sm: ['14px', { lineHeight: '20px' }],
+      base: ['16px', { lineHeight: '20px' }],
+      lg: ['18px', { lineHeight: '28px' }],
+      xl: ['20px', { lineHeight: '24px' }],
+      '2xl': ['24px', { lineHeight: '32px' }],
+      '3xl': ['30px', { lineHeight: '36px' }],
+      '4xl': ['36px', { lineHeight: '40px' }],
+    },
     fill: {
       current: 'currentColor',
     },
