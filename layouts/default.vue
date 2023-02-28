73606439606e711b42d14048cd68089d9edb9fd6
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2023-02-28 09:07:34 -0300

    feat: texts terms

--- layouts/default.vue
+++ layouts/default.vue
@@ -43,7 +43,7 @@ export default {
     return {
       showModal: false,
       showModalPrivacy: false,
-      showModalTerms: false,
+      showModalTerms: true,
       showModalCookies: false,
       acceptedCookies: false,
     }
